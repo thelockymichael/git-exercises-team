12b37d57f787154a5a7732b60606620b220bdfc6
Michael Lock <thelockymichael@gmail.com>
2020-09-06 21:14:05 +0300

    added number counter and button

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import {StatusBar} from 'expo-status-bar'
-import React from 'react'
-import {StyleSheet, Text, View} from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import React, { useState } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native'
 
 export default function App() {
+  const [counter, setCounter] = useState(0)
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+
+      <Text>Counter: {counter}</Text>
+      <Button
+        title="Increase number"
+        onPress={() => setCounter(counter + 1)}
+      />
       <StatusBar style="auto" />
     </View>
   )
